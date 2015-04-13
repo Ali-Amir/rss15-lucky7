a75facc96b6956b36b99f053d35efc9f142ae234
Ali-Amir Aldan <aliamir@mit.edu>
2015-04-12 20:26:14 -0400

    Ignoring auto-generated files

--- src/challenge/rss_msgs/build/generated-src/rss_msgs/ArmMsg.java
+++
@@ -1,8 +0,0 @@
-package rss_msgs;
-
-public interface ArmMsg extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "rss_msgs/ArmMsg";
-  static final java.lang.String _DEFINITION = "int64[8] pwms";
-  long[] getPwms();
-  void setPwms(long[] value);
-}
