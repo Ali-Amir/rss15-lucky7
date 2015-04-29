839c1f5f8b7ad07d760565103285fe389c784c0d
colinhmit <colinh@mit.edu>
2015-04-29 14:57:50 -0400

    velocity testing

--- src/challenge/uorc_listener/src/main/java/com/github/challenge/uorc_listener/MotorListener.java
+++ src/challenge/uorc_listener/src/main/java/com/github/challenge/uorc_listener/MotorListener.java
@@ -16,6 +16,12 @@ public class MotorListener implements MessageListener<MotionMsg> {
     
   @Override
 	public void onNewMessage(MotionMsg msg){
+
+    System.out.println("///////New Motor Message Received!/////////");
+    System.out.println("Translational Vel :");
+    System.out.println(msg.getTranslationalVelocity);
+    System.out.println("Rotational Vel :");
+    System.out.println(msg.getRotationalVelocity);
 	
     double left = msg.getTranslationalVelocity();
     double right = msg.getTranslationalVelocity();
