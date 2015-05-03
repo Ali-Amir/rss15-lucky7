2873ba9de786e66e9565865450bec6c381b1237f
colinhmit <colinh@mit.edu>
2015-05-03 19:57:24 -0400

    my changes

--- src/challenge/arm_control/src/main/java/com/github/challenge/arm_control/Grasping.java
+++ src/challenge/arm_control/src/main/java/com/github/challenge/arm_control/Grasping.java
@@ -984,14 +984,13 @@ public class Grasping extends AbstractNodeMain {
 						(blobTrack.targetBearing*180.0/Math.PI));
 
 				if (Math.abs(blobTrack.rotationVelocityCommand)<0.001 && Math.abs(blobTrack.targetRange-SEARCH_STANDOFF) < EPS_SEARCH_STANDOFF) {
-					//fsmState = RoboFSM.SET_ARM_RETRACTED;
+					//fsmState = RoboFSM.SET_ARM_RETRACTED;<<<<
 					setVelocity(0.0, 0.0);
 				} else {
-					//System.out.println("GRASPING:   trans, rot:" + blobTrack.translationVelocityCommand + ", " +
-							//blobTrack.rotationVelocityCommand);
+			
 					// move robot towards target
 					
-					setVelocity(blobTrack.rotationVelocityCommand, blobTrack.translationVelocityCommand);
+					//setVelocity(blobTrack.rotationVelocityCommand, blobTrack.translationVelocityCommand);<<<
 
 					
 				}
