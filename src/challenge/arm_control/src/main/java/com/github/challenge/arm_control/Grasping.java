84fbf4c63d6335d01d47a101686a3adac4db585b
colinhmit <colinh@mit.edu>
2015-05-03 21:49:04 -0400

    my changes

--- src/challenge/arm_control/src/main/java/com/github/challenge/arm_control/Grasping.java
+++ src/challenge/arm_control/src/main/java/com/github/challenge/arm_control/Grasping.java
@@ -398,7 +398,7 @@ public class Grasping extends AbstractNodeMain {
 						System.out.println("GRASPING: BLADE IS SET TO COLLECT");
 						//fsmState = RoboFSM.ENGAGE_BLOCK;<<
 						fsmState = RoboFSM.BLIND_APPROACH;
-						moveDistance = .19
+						moveDistance = .19;
 					}
 					break;
 				}
